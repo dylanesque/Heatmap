866f6d07e06d1613c650525ced06ac238aa00de8
Michael Caveney <dylanesque27@gmail.com>
2021-04-30 18:03:31 -0400

    added parameter notes

--- chart.js
+++ chart.js
@@ -1,2 +1,21 @@
-const data =
-  'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+async function heatMap() {
+  // 1. Access data
+  const initialData = await d3.json(
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json'
+  );
+
+  const baseTemp = initialData.baseTemperature;
+  // Heat maps have three dimensions: x, y, and a color scale.
+  // x equals year
+  // y equals month
+
+  // 2. Create chart dimensions
+
+  // 3. Draw canvas
+
+  // 4. Create scales
+
+  // 5. Draw data
+}
+
+heatMap();
